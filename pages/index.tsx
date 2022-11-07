09fb8297459050d349120a09bdb64debbffa3eea
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-07 00:18:06 -0300

    exibição de tarefas concluidas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,29 @@ import { useEffect, useState } from "react";
 import { remult } from "remult";
 import { Task } from "../src/shared/Task";
 
-async function fetchTasks() {
-  return remult.repo(Task).find();
+async function fetchTasks(hideCompleted: boolean) {
+  return remult.repo(Task).find({
+    limit: 20,
+    orderBy: { completed: "asc" },
+    where: { completed: hideCompleted ? false : undefined },
+  });
 }
 
 const Home: NextPage = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [hideCompleted, setHideCompleted] = useState(false);
 
   useEffect(() => {
-    fetchTasks().then(setTasks);
-  }, []);
+    fetchTasks(hideCompleted).then(setTasks);
+  }, [hideCompleted]);
   return (
     <div>
+      <input
+        type="checkbox"
+        checked={hideCompleted}
+        onChange={(e) => setHideCompleted(e.target.checked)}
+      />
+      Hide Completed
       <main>
         {tasks.map((task) => (
           <div key={task.id}>
