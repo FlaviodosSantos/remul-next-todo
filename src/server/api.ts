d5abd78c5e56674cdba4141bfc014c88a35e3a5c
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-06 23:57:03 -0300

    definindo modelo e dados de teste de semente

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,3 +1,18 @@
 import { createRemultServer } from "remult/server";
+import { Task } from "../shared/Task";
 
-export const api = createRemultServer({})
+export const api = createRemultServer({
+  entities: [Task],
+  initApi: async (remult) => {
+    const taskRepo = remult.repo(Task);
+    if ((await taskRepo.count()) === 0) {
+      await taskRepo.insert([
+        { title: "Task a" },
+        { title: "Task b", completed: true },
+        { title: "Task c" },
+        { title: "Task d" },
+        { title: "Task e", completed: true },
+      ]);
+    }
+  },
+});
